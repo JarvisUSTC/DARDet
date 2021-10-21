28b66362484e7a9babd2d938ffa9cb438eb6bc4d
v-jianqian <v-jianqian@microsoft.com>
2021-10-21 05:34:51 +0000

    init_on_aml

--- chixma_init_on_aml.sh
+++ chixma_init_on_aml.sh
@@ -25,45 +25,37 @@ sudo ln -sf g++-7 g++
 cd /
 gcc -v
 
+# install mmcv-full
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
 # Install Easydict and other libs
 # pip install numpy==1.15.4 # specific numpy version for PHOTO_METRIC_DISTORT
-pip install easydict cython matplotlib scipy
-pip install opencv-python pyyaml packaging pycocotools tensorboardX cffi shapely
-pip install Polygon3 json_tricks munkres ninja pandas paramiko requests scikit-image tqdm yacs
-pip install transformers==2.11.0
-pip install filelock iopath
+# pip install easydict cython matplotlib scipy
+# pip install opencv-python pyyaml packaging pycocotools tensorboardX cffi shapely
+# pip install Polygon3 json_tricks munkres ninja pandas paramiko requests scikit-image tqdm yacs
+# pip install transformers==2.11.0
+# pip install filelock iopath
 # pip install torch==1.7.0
-pip install timm
+# pip install timm
 
 #Error: mkl-service + Intel(R) MKL: MKL_THREADING_LAYER=INTEL is incompatible with libgomp.so.1 library. Try to import numpy first or set the threading layer accordingly. Set MKL_SERVICE_FORCE_INTEL to force it.
 #solution: https://python.libhunt.com/pytorch-latest-version
 export MKL_THREADING_LAYER=GNU
 
 # make file
-sudo chmod -R 777 /detectron
-cd /detectron
-rm -rf build/ **/*.so
-python -m pip install -e .
-
-cd /detectron/detectron2/utils
-sh make.sh
-
-cd /detectron/detectron2/layers/_cpools
-rm -rf ./build
-rm -rf ./dist
-rm -rf ./cpools.egg-info
-python setup.py install
+sudo chmod -R 777 /mmdetection
+cd /mmdetection
+pip install -r requirements/build.txt
+pip install -v -e .
 
 # link dataset, start use blob, don't link data now
-cd /detectron
+cd /mmdetection
 mkdir -p datasets
 sudo ln -s /blob/data/pretrained_models/detectron2_related_models ./datasets/pretrained_models
 
-sudo mkdir /detectron/Polygon
-sudo cp /blob/data/Polygon3.zip /detectron/Polygon/Polygon.zip
-sudo unzip -d /detectron/Polygon /detectron/Polygon/Polygon.zip
-sudo chmod -R 777 /detectron/Polygon
-cd /detectron/Polygon/Polygon3-3.0.7
-python setup.py install
+#DOTA_devkit
+sudo apt-get install swig
+cd DOTA_devkit/polyiou
+swig -c++ -python csrc/polyiou.i
+python setup.py build_ext --inplace
 
-cd /detectron
+cd /mmdetection
